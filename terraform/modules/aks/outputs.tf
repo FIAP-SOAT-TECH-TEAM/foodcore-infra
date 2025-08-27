3b9ff17178ec119ac54fa80bda385ac38900c556
Pedro Ferrarezzo <ferrarezzolabs@outlook.com>
2025-08-27 19:52:45 -0300

    fix: fix duplicated output aks_name

--- terraform/modules/aks/outputs.tf
+++ terraform/modules/aks/outputs.tf
@@ -2,10 +2,6 @@ output "aks_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
 
-output "aks_name" {
-  value = azurerm_kubernetes_cluster.aks.name
-}
-
 output "aks_resource_group" {
   description = "Resource Group onde o cluster AKS reside"
   value       = azurerm_kubernetes_cluster.aks.resource_group_name
