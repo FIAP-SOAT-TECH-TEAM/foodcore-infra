e4a8e9f2aad06bfe0f02e19654a668e66cfcd994
Pedro Ferrarezzo <ferrarezzolabs@outlook.com>
2025-08-27 19:58:21 -0300

    fix: create aks_subnet and  apim_subnet output

--- terraform/modules/vnet/outputs.tf
+++ terraform/modules/vnet/outputs.tf
@@ -2,3 +2,13 @@ output "aks_subnet_last_usable_ip" {
   description = "Último endereço IP utilizável da subnet do AKS (exclui o IP final reservado e broadcast)."
   value       = cidrhost(azurerm_subnet.aks_subnet.address_prefixes[0], -2)
 }
+
+output "aks_subnet" {
+  description = "Subnet do AKS"
+  value = azurerm_subnet.aks_subnet
+}
+
+output "apim_subnet" {
+  description = "Subnet do APIM"
+  value = azurerm_subnet.apim_subnet
+}
