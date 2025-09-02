a5fc33c00e983996495acd85f70862061e054ce9
Pedro Ferrarezzo <ferrarezzolabs@outlook.com>
2025-09-01 21:21:33 -0300

    fix: use product_id instead product.id in product id output

--- terraform/modules/apim/outputs.tf
+++ terraform/modules/apim/outputs.tf
@@ -15,7 +15,7 @@ output "apim_name" {
 
 output "apim_foodcore_start_productid" {
   description = "ID do produto do API Management"
-  value       = azurerm_api_management_product.foodcoreapi_start_product.id
+  value       = azurerm_api_management_product.foodcoreapi_start_product.product_id
 }
 
 output "apim_foodcore_start_subscriptionid" {
