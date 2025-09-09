fd4e649daf75de9a1c655d3fa04cfd1465de49bd
pedroferrarezzo <ferrarezzolabs@outlook.com>
2025-09-09 17:40:32 -0300

    fix: fix name of graph_directory_read_write_all datasource

--- terraform/modules/app_registration/datasources.tf
+++ terraform/modules/app_registration/datasources.tf
@@ -2,7 +2,7 @@ data "azuread_application" "graph_api_app" {
   client_id = "00000003-0000-0000-c000-000000000000"
 }
 
-data "azuread_application_oauth2_permission_scope" "graph_directory_read_write_all" {
-  application_id = data.azuread_application.graph_api_app.application_id
-  value          = "Directory.ReadWrite.All"
+data "azuread_service_principal_oauth2_permission_scope" "graph_directory_read_write_all" {
+  service_principal_id = data.azuread_application.graph_api_app.service_principal_object_id
+  value                = "Directory.ReadWrite.All"
 }
