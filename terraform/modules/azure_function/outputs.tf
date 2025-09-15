e63a4691d5a1ba53774302588bad2b6548f7a116
pedroferrarezzo <ferrarezzolabs@outlook.com>
2025-09-15 13:35:24 -0300

    fix: fix private_connection_resource_id value

--- terraform/modules/azure_function/outputs.tf
+++ terraform/modules/azure_function/outputs.tf
@@ -1,4 +1,4 @@
 output "azfunc_name" {
   description = "O nome da Azure Function App"
-  value       = azurerm_function_app_flex_consumption.azfunc.name
+  value       = azurerm_linux_function_app.azfunc.name
 }
