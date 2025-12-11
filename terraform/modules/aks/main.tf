6ab06e6cb95a31506255489bc5d1f36b223a9acf
Pedro <ferrarezzolabs@outlook.com>
2025-12-11 13:55:11 -0300

    fix: fix agic_identity call

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -57,21 +57,21 @@ resource "azurerm_role_assignment" "aks_subnet_contributor" {
  resource "azurerm_role_assignment" "agic_reader_rg" {
    scope                = var.resource_group_id
    role_definition_name = "Reader"
-   principal_id         = data.azurerm_user_assigned_identity.ingress.principal_id
+   principal_id         = data.azurerm_user_assigned_identity.agic_identity.principal_id
 
    depends_on = [azurerm_kubernetes_cluster.aks]
  }
  resource "azurerm_role_assignment" "agic_network_contributor_vnet" {
    scope                = var.vnet_id
    role_definition_name = "Network Contributor"
-   principal_id         = data.azurerm_user_assigned_identity.ingress.principal_id
+   principal_id         = data.azurerm_user_assigned_identity.agic_identity.principal_id
 
    depends_on = [azurerm_kubernetes_cluster.aks]
  }
  resource "azurerm_role_assignment" "agic_contributor_appgw" {
    scope                = var.appgw_id
    role_definition_name = "Contributor"
-   principal_id         = data.azurerm_user_assigned_identity.ingress.principal_id
+   principal_id         = data.azurerm_user_assigned_identity.agic_identity.principal_id
 
    depends_on = [azurerm_kubernetes_cluster.aks]
  }
