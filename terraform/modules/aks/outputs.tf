866887bef4995aea3116c8805853889f4a7b7146
Pedro <ferrarezzolabs@outlook.com>
2025-12-18 21:50:55 -0300

    chore: trigger ci

--- terraform/modules/aks/outputs.tf
+++ terraform/modules/aks/outputs.tf
@@ -11,3 +11,8 @@ output "aks_secret_identity_client_id" {
   description = "Client ID da identidade gerenciada do tipo UserAssigned criada para o Azure Key Vault Secrets Provider."
   value       = local.secrets_store_csi_identity_client_id
 }
+
+output "kube_config" {
+  description = "Kube config do cluster AKS"
+  value       = azurerm_kubernetes_cluster.aks.kube_config
+}
