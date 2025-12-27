1a4da23f5e16b43cc326c2dfa09c459438e6e42d
Pedro <ferrarezzolabs@outlook.com>
2025-12-27 20:33:35 -0300

    chore: trigger ci

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -165,7 +165,7 @@
   variable "aks_availability_zones" {
     type        = list(string)
     description = "Zonas de disponibilidade para o pool de nós do AKS"
-    default = [ "3" ]
+    default = [ "2", "3" ]
   }
   variable "vm_size" {
     type    = string
